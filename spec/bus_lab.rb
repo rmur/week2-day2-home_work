6d7e34b065566fde9272b131bf8707168840fc23
R.Murzakov <romuald.murzakov@gmail.com>
2017-07-04 15:16:37 +0100

    Part A complete

--- spec/bus_lab.rb
+++ spec/bus_lab.rb
@@ -4,11 +4,28 @@ require_relative('../person.rb')
 
 class TestBusDriving < MiniTest::Test
 
-def setup
+  def setup
 
-@bus = Bus.new(22 , "Ocean Terminal")
-@passenger = Passenger.new("John", 23)
+    @bus = Bus.new(22 , "Ocean Terminal")
+    @passenger = Passenger.new("John", 23)
 
-end
+  end
+
+  def test_bus_route
+    bus = @bus.route_number
+    assert_equal(22, bus)
+    
+  end
+
+  def test_bus_sound
+    assert_equal("Wrooom", @bus.drive)
+  end
+
+  def test_passanger_name_and_age
+    person_age = @passenger.age
+    person_name = @passenger.name
+    assert_equal(23 , person_age)
+    assert_equal("John", person_name)
+  end
 
 end
